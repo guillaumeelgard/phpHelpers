402528253e2e802494d11948dd03053c7ca10cf8
Guillaume ELGARD <guillaume.elgard@yachtsolutions.fr>
2022-06-02 12:24:28 +0200

    Amélioration du debug

--- helpers/debug.php
+++ helpers/debug.php
@@ -5,220 +5,36 @@ use Bokad\Helpers\Debug;
 if (!function_exists('explicit_var')) {
     function explicit_var(mixed $var): string
     {
-        switch (gettype($var)) {
-            case 'object':
-            case 'array':
-                return print_r($var, true);
-
-            case 'string':
-                return '(string ' . mb_strlen($var) . ') "' . $var . '"';
-
-            case 'integer':
-            case 'double':
-                return '(' . gettype($var) . ') ' . $var;
-
-            case 'boolean':
-                return '(boolean) ' . ($var ? 'true' : 'false');
-
-            case 'NULL':
-                return 'NULL';
-
-            default:
-                throw new \Exception('Type ' . gettype($var) . ' non géré.');
-        }
-    }
-}
-
-if (!function_exists('pp')) {
-    function pp(
-        array $varsArray,
-        ?int $displayDebugBacktrace = null,
-        ?string $tag = null,
-        int $offset = 0,
-        ?string $mode = null,
-        bool $explicit_var = true,
-    ): void {
-        if (is_null($displayDebugBacktrace)) {
-            $displayDebugBacktrace = Debug::getDefaultDisplayDebugBacktracePP();
-        }
-
-        $preStyle = [
-            'background' => Debug::getColorBack(),
-            'color' => Debug::getColorText(),
-            'padding' => '1em',
-            'white-space' => 'break-spaces',
-            'border' => 'dashed 1px ' . Debug::getColorHigh(),
-        ];
-
-        $hrStyle = [
-            'border' => 'none',
-            'border-top' => 'dashed 1px ' . Debug::getColorText(),
-            'margin' => '2em 0',
-        ];
-
-        $tagStyle = [
-            'display' => 'inline-block',
-            'background' => 'teal',
-            'color' => Debug::getColorBack(),
-            'padding' => '.5em',
-            'font-size' => '1.1em',
-        ];
-
-        if (!in_array($mode, ['html', 'text'])) {
-            $mode = Debug::getDefaultMode();
-        }
-
-        $textLine = '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-';
-
-        switch ($mode) {
-            case 'html':
-                echo '<pre ' . styleFromArray($preStyle) . '>';
-                break;
-
-            case 'text':
-                echo "\n";
-                echo $textLine;
-                echo "\n";
-                echo $textLine;
-                echo "\n";
-                echo "\n";
-                break;
-        }
-
-        if ($tag) {
-            switch ($mode) {
-                case 'html':
-                    echo '<span ' . styleFromArray($tagStyle) . '>';
-                    echo $tag;
-                    echo '</span>';
-                    break;
-
-                case 'text':
-                    echo ' ' . str_pad('', mb_strlen($tag) + 6, '@');
-                    echo "\n";
-                    echo ' @  ' . str_pad('', mb_strlen($tag), ' ') . '  @';
-                    echo "\n";
-                    echo ' @  ' . $tag . '  @';
-                    echo "\n";
-                    echo ' @  ' . str_pad('', mb_strlen($tag), ' ') . '  @';
-                    echo "\n";
-                    echo ' ' . str_pad('', mb_strlen($tag) + 6, '@');
-                    break;
-            }
-
-            echo "\n";
-            echo "\n";
-        }
-
-        if ($displayDebugBacktrace) {
-            $display = [];
-            $db = debug_backtrace();
-
-            if (!isset($db[$offset])) {
-                throw new \Exception('Invalid offset');
-            }
-
-            $display[] = $db[$offset]['file'] . ':' . $db[$offset]['line'];
-            $display[] = '';
-
-            switch ($displayDebugBacktrace) {
-                case 1:
-                    if (array_key_exists($offset + 1, $db)) {
-                        $dbKeys = [$offset + 1];
-                    } else {
-                        $dbKeys = [];
-                    }
-                    break;
-
-                case 2:
-                    $dbKeys = range($offset + 1, count($db) - 1);
-                    break;
-
-                default:
-                    throw new \Exception('Invalid $displayDebugBacktrace value');
-            }
-
-            foreach ($dbKeys as $k) {
-                $display[] = $db[$k]['file'] . ':' . $db[$k]['line'];
-                $display[] = (array_key_exists('class', $db[$k]) ? $db[$k]['class'] . '::' : '') . $db[$k]['function'] . '()';
-                $display[] = '';
-            }
-
-            if ($mode == 'html') {
-                echo '<span style="color:' . Debug::getColorHigh() . '">';
-            }
-
-            echo implode("\n", $display);
-
-            if ($mode == 'html') {
-                echo '</span>';
-            }
-
-            switch ($mode) {
-                case 'html':
-                    echo '<hr ' . styleFromArray(array_merge($hrStyle, ['border-top' => 'dashed 1px ' . Debug::getColorHigh()])) . '>';
-                    break;
-
-                case 'text':
-                    echo "\n";
-                    echo $textLine;
-                    echo "\n";
-                    echo "\n";
-                    break;
-            }
-        }
-
-        if ($explicit_var) {
-            $varsArray = array_map(fn ($a) => explicit_var($a), $varsArray);
-        }
-
-        switch ($mode) {
-            case 'html':
-                echo implode('<hr ' . styleFromArray($hrStyle) . '>', $varsArray);
-                echo '</pre>';
-                break;
-
-            case 'text':
-                echo implode("\n\n" . $textLine . "\n\n", $varsArray);
-                echo "\n";
-                echo $textLine;
-                echo "\n";
-                echo $textLine;
-                echo "\n";
-                break;
-        }
+        return Debug::explicit_var($var);
     }
 }
 
 if (!function_exists('p')) {
     function p(mixed ...$vars): void
     {
-        pp(func_get_args());
+        (new \ReflectionClass('Bokad\Helpers\Debug'))->getMethod('p')->invokeArgs(null, func_get_args());
+    }
+}
+
+if (!function_exists('pp')) {
+    function pp(array $varsArray = [], array $options = []): void
+    {
+        (new \ReflectionClass('Bokad\Helpers\Debug'))->getMethod('pp')->invokeArgs(null, func_get_args());
     }
 }
 
 if (!function_exists('d')) {
     function d(mixed ...$vars): never
     {
-        pp(func_get_args(), Debug::getDefaultDisplayDebugBacktraceDD(), null, 1);
-        exit;
+        Debug::moreOffset();
+        (new \ReflectionClass('Bokad\Helpers\Debug'))->getMethod('d')->invokeArgs(null, func_get_args());
     }
 }
 
 if (!function_exists('dd')) {
-    function dd(
-        array $varsArray,
-        ?int $displayDebugBacktrace = null,
-        ?string $tag = null,
-        int $offset = 0,
-        ?string $mode = null,
-        bool $explicit_var = true,
-    ): never {
-        if (is_null($displayDebugBacktrace)) {
-            $displayDebugBacktrace = Debug::getDefaultDisplayDebugBacktraceDD();
-        }
-
-        pp($varsArray, $displayDebugBacktrace, $tag, $offset + 1, $mode, $explicit_var);
-        exit;
+    function dd(array $varsArray = [], array $options = []): never
+    {
+        Debug::moreOffset();
+        (new \ReflectionClass('Bokad\Helpers\Debug'))->getMethod('dd')->invokeArgs(null, func_get_args());
     }
 }
--- src/Debug.php
+++ src/Debug.php
@@ -4,96 +4,153 @@ namespace Bokad\Helpers;
 
 class Debug
 {
-    private static string $defaultPostMode = 'html';
-    private static string $colorHigh = '#00d3d3';
-    private static string $colorText = '#bfbfbf';
-    private static string $colorBack = '#181818';
-    private static int $defaultDisplayDebugBacktracePP = 0;
-    private static int $defaultDisplayDebugBacktraceD = 1;
-    private static int $defaultDisplayDebugBacktraceDD = 2;
+    private const defaultPostMode = 'html';
+    private static string $postMode;
 
-    public static function setDefaultPostMode(string $mode): void
+    private const defaultColorBack = '#181818';
+    private const defaultColorText = '#bfbfbf';
+    private const defaultColorHigh = '#00d3d3';
+    private static string $colorBack;
+    private static string $colorText;
+    private static string $colorHigh;
+    private const colorList = ['black','silver','gray','white','maroon','red','purple','fuchsia','green','lime','olive','yellow','navy','blue','teal','aqua','aliceblue','antiquewhite','aqua','aquamarine','azure','beige','bisque','black','blanchedalmond','blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkgrey','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkslategrey','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dimgrey','dodgerblue','firebrick','floralwhite','forestgreen','fuchsia','gainsboro','ghostwhite','gold','goldenrod','gray','green','greenyellow','grey','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgray','lightgreen','lightgrey','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightslategrey','lightsteelblue','lightyellow','lime','limegreen','linen','magenta','maroon','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','navy','oldlace','olive','olivedrab','orange','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','purple','red','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','silver','skyblue','slateblue','slategray','slategrey','snow','springgreen','steelblue','tan','teal','thistle','tomato','turquoise','violet','wheat','white','whitesmoke','yellow','yellowgreen'];
+
+    private const defaultDisplayDebugBacktracePP = 0;
+    private const defaultDisplayDebugBacktraceD = 1;
+    private const defaultDisplayDebugBacktraceDD = 2;
+    private static int $displayDebugBacktracePP;
+    private static int $displayDebugBacktraceD;
+    private static int $displayDebugBacktraceDD;
+
+    private static int $offset = 0;
+
+    private static function verifyMode(mixed $mode): bool
     {
-        if (!in_array($mode, ['html', 'text'])) {
+        return in_array($mode, ['html', 'text']);
+    }
+
+    public static function setPostMode(string $mode): void
+    {
+        if (!self::verifyMode($mode)) {
             throw new \Exception('Invalid mode.');
         }
 
-        self::$defaultPostMode = $mode;
+        self::$postMode = $mode;
     }
 
-    public static function getColorHigh(): string
+    public static function moreOffset(): void
     {
-        return self::$colorHigh;
+        static::$offset++;
     }
 
-    public static function setColorHigh(string $color): void
+    private static function verifyColor(mixed $color): bool
     {
-        self::$colorHigh = $color;
-    }
-
-    public static function getColorText(): string
-    {
-        return self::$colorText;
-    }
-
-    public static function setColorText(string $color): void
-    {
-        self::$colorText = $color;
-    }
-
-    public static function getColorBack(): string
-    {
-        return self::$colorBack;
+        return
+            is_string($color)
+            &&
+            (
+                preg_match('/^\#[0-9a-fA-F]{3}$/', $color)
+                ||
+                preg_match('/^\#[0-9a-fA-F]{4}$/', $color)
+                ||
+                preg_match('/^\#[0-9a-fA-F]{6}$/', $color)
+                ||
+                preg_match('/^\#[0-9a-fA-F]{8}$/', $color)
+                ||
+                in_array(mb_strtolower($color), static::colorList)
+            );
     }
 
     public static function setColorBack(string $color): void
     {
+        if (!self::verifyColor($color)) {
+            throw new \Exception('Invalid color.');
+        }
+
         self::$colorBack = $color;
     }
 
-    public static function getDefaultDisplayDebugBacktracePP(): int
+    private static function getColorBack(): string
     {
-        return self::$defaultDisplayDebugBacktracePP;
+        return self::$colorBack ?? self::defaultColorBack;
     }
 
-    public static function setDefaultDisplayDebugBacktracePP(int $value): void
+    public static function setColorText(string $color): void
     {
-        if (!in_array($value, [0, 1, 2])) {
-            throw new \Exception('Invalid value.');
+        if (!self::verifyColor($color)) {
+            throw new \Exception('Invalid color.');
         }
 
-        self::$defaultDisplayDebugBacktracePP = $value;
+        self::$colorText = $color;
     }
 
-    public static function getDefaultDisplayDebugBacktraceD(): int
+    private static function getColorText(): string
     {
-        return self::$defaultDisplayDebugBacktraceD;
+        return self::$colorText ?? self::defaultColorText;
     }
 
-    public static function setDefaultDisplayDebugBacktraceD(int $value): void
+    public static function setColorHigh(string $color): void
     {
-        if (!in_array($value, [0, 1, 2])) {
-            throw new \Exception('Invalid value.');
+        if (!self::verifyColor($color)) {
+            throw new \Exception('Invalid color.');
         }
 
-        self::$defaultDisplayDebugBacktraceD = $value;
+        self::$colorHigh = $color;
     }
 
-    public static function getDefaultDisplayDebugBacktraceDD(): int
+    private static function getColorHigh(): string
     {
-        return self::$defaultDisplayDebugBacktraceDD;
+        return self::$colorHigh ?? self::defaultColorHigh;
     }
 
-    public static function setDefaultDisplayDebugBacktraceDD(int $value): void
+    private static function verifyDisplayDebugBacktrace(mixed $debugBacktrace): bool
     {
-        if (!in_array($value, [0, 1, 2])) {
-            throw new \Exception('Invalid value.');
+        return in_array($debugBacktrace, [0, 1, 2]);
+    }
+
+    public static function setDisplayDebugBacktracePP(int $value): void
+    {
+        if (!self::verifyDisplayDebugBacktrace($value)) {
+            throw new \Exception('Invalid displayDebugBacktrace value.');
         }
 
-        self::$defaultDisplayDebugBacktraceDD = $value;
+        self::$displayDebugBacktracePP = $value;
     }
 
-    public static function getDefaultMode(): string
+    private static function getDisplayDebugBacktracePP(): int
+    {
+        return self::$displayDebugBacktracePP ?? self::defaultDisplayDebugBacktracePP;
+    }
+
+    public static function setDisplayDebugBacktraceD(int $value): void
+    {
+        if (!self::verifyDisplayDebugBacktrace($value)) {
+            throw new \Exception('Invalid displayDebugBacktrace value.');
+        }
+
+        self::$displayDebugBacktraceD = $value;
+    }
+
+    private static function getDisplayDebugBacktraceD(): int
+    {
+        return self::$displayDebugBacktraceD ?? self::defaultDisplayDebugBacktraceD;
+    }
+
+    public static function setDisplayDebugBacktraceDD(int $value): void
+    {
+        if (!self::verifyDisplayDebugBacktrace($value)) {
+            throw new \Exception('Invalid displayDebugBacktrace value.');
+        }
+
+        self::$displayDebugBacktraceDD = $value;
+    }
+
+    private static function getDisplayDebugBacktraceDD(): int
+    {
+        return self::$displayDebugBacktraceDD ?? self::defaultDisplayDebugBacktraceDD;
+    }
+
+    private static function getMode(): string
     {
         if (substr(php_sapi_name(), 0, 3) == 'cli') {
             return 'text';
@@ -104,9 +161,248 @@ class Debug
         }
 
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-            return self::$defaultPostMode;
+            return self::$postMode ?? self::defaultPostMode;
         }
 
         return 'html';
+    }
+
+    private static function getDisplayDebugBacktrace(int $displayDebugBacktrace, int $offset): array
+    {
+        if ($displayDebugBacktrace == 0) {
+            return [];
+        }
+
+        $display = [];
+        $db = debug_backtrace();
+        $offset++;
+
+        if (!isset($db[$offset])) {
+            throw new \Exception('Invalid offset');
+        }
+
+        if (isset($db[$offset]['file'])) {
+            $display[] = $db[$offset]['file'] . ':' . $db[$offset]['line'];
+            $display[] = '';
+        }
+
+        switch ($displayDebugBacktrace) {
+            case 1:
+                if (array_key_exists($offset + 1, $db)) {
+                    $dbKeys = [$offset + 1];
+                } else {
+                    $dbKeys = [];
+                }
+                break;
+
+            case 2:
+                $dbKeys = range($offset + 1, count($db) - 1);
+                break;
+
+            default:
+                throw new \Exception('Invalid $displayDebugBacktrace value');
+        }
+
+        foreach ($dbKeys as $k) {
+            if (isset($db[$k]['file'])) {
+                $display[] = $db[$k]['file'] . ':' . $db[$k]['line'];
+            }
+            $display[] = (array_key_exists('class', $db[$k]) ? $db[$k]['class'] . '::' : '') . $db[$k]['function'] . '()';
+            $display[] = '';
+        }
+
+        return $display;
+    }
+
+    public static function explicit_var(mixed $var): string
+    {
+        switch (gettype($var)) {
+            case 'object':
+            case 'array':
+                return print_r($var, true);
+
+            case 'string':
+                return '(string ' . mb_strlen($var) . ') "' . $var . '"';
+
+            case 'integer':
+            case 'double':
+                return '(' . gettype($var) . ') ' . $var;
+
+            case 'boolean':
+                return '(boolean) ' . ($var ? 'true' : 'false');
+
+            case 'NULL':
+                return 'NULL';
+
+            default:
+                throw new \Exception('Type ' . gettype($var) . ' non géré.');
+        }
+    }
+
+    private static function verifyTag(mixed $tag): bool
+    {
+        return is_scalar($tag) && !is_bool($tag);
+    }
+
+    private static function verifyOffset(mixed $offset): bool
+    {
+        return is_int($offset) && $offset > -1;
+    }
+
+    public static function p(mixed ...$vars): void
+    {
+        self::pp(func_get_args());
+    }
+
+    public static function pp(array $vars = [], array $options = []): void
+    {
+        $mode = isset($options['mode']) && self::verifyMode($options['mode']) ? $options['mode'] : self::getMode();
+        $tag = isset($options['tag']) && self::verifyTag($options['tag']) ? $options['tag'] : null;
+
+        $displayDebugBacktrace = isset($options['displayDebugBacktrace']) && self::verifyDisplayDebugBacktrace($options['displayDebugBacktrace']) ? $options['displayDebugBacktrace'] : self::getDisplayDebugBacktracePP();
+        $offset = isset($options['offset']) && self::verifyOffset($options['offset']) ? $options['offset'] : 0;
+        $offset+= self::$offset;
+        self::$offset = 0;
+        $backtrace = self::getDisplayDebugBacktrace($displayDebugBacktrace, $offset);
+
+        if (!isset($options['explicit_var']) || $options['explicit_var']) {
+            $vars = array_map(fn ($a) => Debug::explicit_var($a), $vars);
+        }
+
+        switch ($mode) {
+            case 'html':
+                $colorBack = isset($options['colorBack']) && self::verifyColor($options['colorBack']) ? $options['colorBack'] : self::getColorBack();
+                $colorText = isset($options['colorText']) && self::verifyColor($options['colorText']) ? $options['colorText'] : self::getColorText();
+                $colorHigh = isset($options['colorHigh']) && self::verifyColor($options['colorHigh']) ? $options['colorHigh'] : self::getColorHigh();
+                self::ppHtml($vars, $tag, $backtrace, $colorBack, $colorText, $colorHigh);
+                break;
+
+            case 'text':
+                self::ppText($vars, $tag, $backtrace);
+                break;
+        }
+    }
+
+    public static function d(mixed ...$vars): never
+    {
+        self::moreOffset();
+        self::pp(func_get_args(), [
+            'displayDebugBacktrace' => self::getDisplayDebugBacktraceD(),
+        ]);
+        exit;
+    }
+
+    public static function dd(array $vars = [], array $options = []): never
+    {
+        $options = array_merge(
+            [
+                'displayDebugBacktrace' => self::getDisplayDebugBacktraceDD(),
+            ],
+            $options
+        );
+
+        if (!static::verifyDisplayDebugBacktrace($options['displayDebugBacktrace'])) {
+            $options['displayDebugBacktrace'] = self::getDisplayDebugBacktraceDD();
+        }
+
+        self::moreOffset();
+        self::pp($vars, $options);
+        exit;
+    }
+
+    private static function ppHtml(
+        array $vars,
+        ?string $tag,
+        array $backtrace,
+        string $colorBack,
+        string $colorText,
+        string $colorHigh,
+    ): void {
+        $preStyle = [
+            'background' => $colorBack,
+            'color' => $colorText,
+            'padding' => '1em',
+            'white-space' => 'break-spaces',
+            'border' => 'dashed 1px ' . $colorHigh,
+        ];
+
+        $tagStyle = [
+            'display' => 'inline-block',
+            'background' => $colorHigh,
+            'color' => $colorBack,
+            'padding' => '.5em',
+            'font-size' => '1.1em',
+        ];
+
+        $hrStyle = [
+            'border' => 'none',
+            'border-top' => 'dashed 1px ' . $colorText,
+            'margin' => '2em 0',
+        ];
+
+        echo '<pre ' . styleFromArray($preStyle) . '>';
+
+        if ($tag) {
+            echo '<span ' . styleFromArray($tagStyle) . '>';
+            echo $tag;
+            echo '</span>';
+            echo "\n";
+            echo "\n";
+        }
+
+        if (!empty($backtrace)) {
+            echo '<span style="color:' . $colorHigh . '">';
+            echo implode("\n", $backtrace);
+            echo '</span>';
+            echo '<hr ' . styleFromArray(array_merge($hrStyle, ['border-top' => 'dashed 1px ' . $colorHigh])) . '>';
+        }
+
+        echo implode('<hr ' . styleFromArray($hrStyle) . '>', $vars);
+        echo '</pre>';
+    }
+
+    private static function ppText(
+        array $vars,
+        ?string $tag,
+        array $backtrace,
+    ): void {
+        $textLine = '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-';
+
+        echo "\n";
+        echo $textLine;
+        echo "\n";
+        echo $textLine;
+        echo "\n";
+        echo "\n";
+
+        if ($tag) {
+            echo ' ' . str_pad('', mb_strlen($tag) + 6, '@');
+            echo "\n";
+            echo ' @  ' . str_pad('', mb_strlen($tag), ' ') . '  @';
+            echo "\n";
+            echo ' @  ' . $tag . '  @';
+            echo "\n";
+            echo ' @  ' . str_pad('', mb_strlen($tag), ' ') . '  @';
+            echo "\n";
+            echo ' ' . str_pad('', mb_strlen($tag) + 6, '@');
+            echo "\n";
+            echo "\n";
+        }
+
+        if (!empty($backtrace)) {
+            echo implode("\n", $backtrace);
+            echo "\n";
+            echo $textLine;
+            echo "\n";
+            echo "\n";
+        }
+
+        echo implode("\n\n" . $textLine . "\n\n", $vars);
+        echo "\n";
+        echo "\n";
+        echo $textLine;
+        echo "\n";
+        echo $textLine;
+        echo "\n";
     }
 }
